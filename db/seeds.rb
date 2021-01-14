c4350d35f29907dd64def74c4940aba7a2c9703c
SamGarner <samuel.w.garner@gmail.com>
2021-01-14 13:41:17 -0600

    Style: remove extra spacing and add comment to db seeds.rb file

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,10 +8,10 @@
 
 Airport.destroy_all # only since playground app
 Airport.create([{ id: 1, code: 'MDW', city: 'Chicago' }, { id: 2,  code: 'DEN', city: 'Denver' },
-                { id: 3, code: 'VRN',  city: 'Verona, IT' }, { id: 4, code: 'HND', city: 'Tokyo, JP' },
+                { id: 3, code: 'VRN', city: 'Verona, IT' }, { id: 4, code: 'HND', city: 'Tokyo, JP' },
                 { id: 5, code: 'IST', city: 'Istanbul, TR' }])
 
-Flight.destroy_all
+Flight.destroy_all # only since playground app
 Flight.create([{ duration: 439, take_off: '2011-01-05 01:00:00 UTC', departure_id: 1, arrival_id: 3 },
                { duration: 154, take_off: '2021-05-05 01:00:00 UTC', departure_id: 3, arrival_id: 5 },
                { duration: 597, take_off: '2022-09-13 09:22:10 UTC', departure_id: 4, arrival_id: 2 },
