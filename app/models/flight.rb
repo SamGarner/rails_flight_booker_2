78e8c1a80c331f08c7d44c721b8f9a002ca45b47
SamGarner <samuel.w.garner@gmail.com>
2021-01-14 14:12:51 -0600

    Feat: Add method to Flight model to better format take_off DateTime.

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -3,4 +3,8 @@ class Flight < ApplicationRecord
 
   belongs_to :departure, class_name: 'Airport'
   belongs_to :arrival, class_name: 'Airport'
+
+  def formatted_date(datetime = take_off)
+    datetime.strftime('%F at %H:%M')
+  end
 end
