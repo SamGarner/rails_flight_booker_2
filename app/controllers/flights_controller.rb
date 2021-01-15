a1689e90279eb6910c75434b36ac421ff8968dde
SamGarner <samuel.w.garner@gmail.com>
2021-01-14 19:37:32 -0600

    Feat: Searchable by departure airport

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,6 +1,12 @@
 class FlightsController < ApplicationController
   def index
-    @flights = Flight.all
+    # @flights = Flight.search(params[:departure_id]) #, params[:arrival_id], 
+                # params[:take_off], params[:passenger_count]])
+    @flights = Flight.search(params[:departure_id])
+    @departure_options = Flight.all.map { |d| [d.departure.city, d.departure_id] }
+    @arrival_options = Flight.all.map { |a| [a.arrival.city, a.arrival_id] }
+    @date_options = Flight.all.map { |date| [date.formatted_date, date] }
+    @passenger_count_options = [[1, 1], [2, 2], [3, 3], [4, 4]]
   end
 
   def new
@@ -10,5 +16,11 @@ class FlightsController < ApplicationController
   end
 
   def create
+  end
+
+  private
+
+  def allowed_flight_params
+    params.require(:flight).permit(:departure_id, :arrival_id, :take_off, :passenger_count)
   end
 end
--- app/models/flight.rb
+++ app/models/flight.rb
@@ -7,4 +7,22 @@ class Flight < ApplicationRecord
   def formatted_date(datetime = take_off)
     datetime.strftime('%F at %H:%M')
   end
+
+  def self.search(allowed_flight_params)
+    # flight_params = [departure_id, arrival_id, take_off, passenger_count]
+    
+    # if allowed_flight_params == []
+    #   @flights = Flight.all
+    # else
+
+      # @flights = Flight.where('departure_id = ? AND arrival_id = ? AND take_off = ?
+      #   AND passenger_count = ?', params[:departure_id], params[:arrival_id], 
+      #   params[:take_off], params[:passenger_count])
+      
+      # @flights = Flight.where('departure_id = ?', params[:departure_id])
+      @flights = []
+      @flights << Flight.find_by(departure_id: allowed_flight_params)
+      @flights = @flights || Flight.all
+    # end
+  end
 end
