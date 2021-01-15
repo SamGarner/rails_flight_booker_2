7742f9ee50a8ca074be9bab2b6f675c494ec3de8
SamGarner <samuel.w.garner@gmail.com>
2021-01-15 14:31:00 -0600

    Feat: Init app copy

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 Airport.destroy_all # only since playground app
-Airport.create([{ id: 1, code: 'MDW', city: 'Chicago' }, { id: 2,  code: 'DEN', city: 'Denver' },
+Airport.create([{ id: 1, code: 'MDW', city: 'Chicago' }, { id: 2, code: 'DEN', city: 'Denver' },
                 { id: 3, code: 'VRN', city: 'Verona, IT' }, { id: 4, code: 'HND', city: 'Tokyo, JP' },
                 { id: 5, code: 'IST', city: 'Istanbul, TR' }])
 
@@ -16,4 +16,4 @@ Flight.create([{ duration: 439, take_off: '2011-01-05 01:00:00 UTC', departure_i
                { duration: 154, take_off: '2021-05-05 01:00:00 UTC', departure_id: 3, arrival_id: 5 },
                { duration: 597, take_off: '2022-09-13 09:22:10 UTC', departure_id: 4, arrival_id: 2 },
                { duration: 123, take_off: '2020-09-22 18:20:00 UTC', departure_id: 2, arrival_id: 1 },
-               { duration: 692, take_off: '2011-04-30 11:00:00 UTC' , departure_id: 1, arrival_id: 5 }])
+               { duration: 692, take_off: '2011-04-30 11:00:00 UTC', departure_id: 1, arrival_id: 5 }])
